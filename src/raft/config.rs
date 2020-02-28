0374a1fab157ddff31a0e232ebfeee2c60ba7ce9
fall snow <snowfallvilla@outlook.com>
2020-02-28 14:54:31 +0800

    1. add rustfmt.toml and format code

--- src/raft/config.rs
+++ src/raft/config.rs
@@ -1,9 +1,11 @@
 extern crate rand;
 use base64_url::*;
+use chrono::{DateTime, Local};
 use num_bigint::{BigInt, RandBigInt, ToBigInt};
-use num_traits::{Zero,cast::ToPrimitive};
+use num_traits::{cast::ToPrimitive, Zero};
 use rand::{random, thread_rng, Rng};
-
+use std::collections::HashMap;
+use std::sync::Mutex;
 pub fn rand_string(n: usize) -> String {
     let mut b = vec![0u8; 2 * n];
     let mut rng = rand::thread_rng();
@@ -28,6 +30,48 @@ fn make_seed() -> i64 {
     } else {
         0
     }
+}
+
+/*
+type config struct {
+    mu        sync.Mutex
+    t         *testing.T
+    net       *labrpc.Network
+    n         int
+    rafts     []*Raft
+    applyErr  []string // from apply channel readers
+    connected []bool   // whether each server is on the net
+    saved     []*Persister
+    endnames  [][]string            // the port file names each sends to
+    logs      []map[int]interface{} // copy of each server's committed entries
+    start     time.Time             // time at which make_config() was called
+    // begin()/end() statistics
+    t0        time.Time // time at which test_test.go called cfg.begin()
+    rpcs0     int       // rpcTotal() at start of test
+    cmds0     int       // number of agreements
+    bytes0    int64
+    maxIndex  int
+    maxIndex0 int
+}
+*/
+pub struct Raft(i32);
+pub struct Persister(i32);
+pub struct Config {
+    mu:          Box<Mutex<u32>>,
+    n:           i32,
+    rafts:       Vec<Box<Raft>>,
+    apply_err:   Vec<String>,
+    connected:   Vec<bool>,
+    saved:       Vec<Box<Persister>>,
+    end_name:    Vec<Vec<String>>,
+    logs:        Vec<HashMap<i32, i32>>, // copy of each server's committed entries
+    start:       chrono::DateTime<Local>, // time at which make_config() was called
+    t0:          chrono::DateTime<Local>,
+    rpcs0:       i32,
+    cmds0:       i32,
+    bytes0:      i64,
+    max_index:   i64,
+    max_index_0: i64,
 }
 
 #[test]
